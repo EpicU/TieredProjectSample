5fa5da3dab951eba3a65eff8b1b6c81a924e6a49
David Thompson <davidt@exigo.com>
2016-03-16 18:19:00 -0500

    Initial commit

---
+++ MyShop.Data/Repository.cs
@@ -0,0 +1,22 @@
+﻿using MyShop.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Data
+{
+    public class Repository
+    {
+        public static List<Customer> GetCustomers()
+        {
+            return new List<Customer>()
+            {
+                new Customer() {  CustomerId = 1, Name = "John" },
+                new Customer() {  CustomerId = 2, Name = "Jane" },
+                new Customer() {  CustomerId = 3, Name = "Foo" }
+            };
+        }
+    }
+}
---
+++ MyShop.Service/CustomerService.cs
@@ -0,0 +1,34 @@
+﻿using MyShop.Core;
+using MyShop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Service
+{
+    public class CustomerService
+    {
+        public List<Customer> GetCustomers()
+        {
+            var customers = Repository.GetCustomers();
+
+            foreach(var customer in customers)
+            {
+                if(customer.CustomerId == 1)
+                {
+                    customer.Name = "Idiot";
+                }
+            }
+
+            return customers;
+        }
+
+        public Customer GetCustomer(int customerId)
+        {
+            var customers = Repository.GetCustomers();
+            return customers.FirstOrDefault(c => c.CustomerId == customerId);
+        }
+    }
+}
